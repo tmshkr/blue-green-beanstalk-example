5c5e66efcd88eeefce6f064e6e402d59dfa5be2c
Tim Shaker <tim@tmshkr.com>
2024-12-22 14:39:57 -0800

    update print-env-info.sh

--- .platform/hooks/prebuild/print-env-info.sh
+++ .platform/hooks/prebuild/print-env-info.sh
@@ -1,28 +1,17 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
-if [ ! $IS_DEVELOPMENT ]
-then
-  cfn=/opt/elasticbeanstalk/deployment/cfn-metadata-cache.json
-  version=/opt/elasticbeanstalk/deployment/app_version_manifest.json
-  
-  export ENVIRONMENT_ID=$(cat $cfn | jq -r '.EbResource."AWS::ElasticBeanstalk::Metadata".EnvironmentId')
-  export ENVIRONMENT_NAME=$(cat $cfn | jq -r '.EbResource."AWS::ElasticBeanstalk::Metadata".EnvironmentName')
-  export VERSION_LABEL=$(cat $version | jq -r '.VersionLabel')
-else
-  export ENVIRONMENT_ID="dev-env-id"
-  export ENVIRONMENT_NAME="dev-env-blue"
-  export VERSION_LABEL="dev-version"
-fi
+export ENVIRONMENT_ID=$(cat /opt/elasticbeanstalk/config/ebenvinfo/envid)
+env=$(aws elasticbeanstalk describe-environments --environment-ids $ENVIRONMENT_ID)
+export ENVIRONMENT_NAME=$(echo $env | jq -r '.Environments[0].EnvironmentName')
+export VERSION_LABEL=$(echo $env | jq -r '.Environments[0].VersionLabel')
 
 if [[ $ENVIRONMENT_NAME == *blue* ]]; then
-    export BG_COLOR="blue"
+  export BG_COLOR="blue"
 elif [[ $ENVIRONMENT_NAME == *green* ]]; then
-    export BG_COLOR="green"
+  export BG_COLOR="green"
 else
-    export BG_COLOR=""
+  export BG_COLOR=""
 fi
 
-
 mkdir -p public
-envsubst < index.template.html > public/index.html
+envsubst <index.template.html >public/index.html
